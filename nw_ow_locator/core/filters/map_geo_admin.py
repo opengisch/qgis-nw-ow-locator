93f8bfb15a0e8e5d0e0f88eed786a1894fdb4c53
Pi Moll <patricia@opengis.ch>
2026-09-08 09:01:41 +0200

    Some more background info for the sortbox parameter

--- nw_ow_locator/core/filters/map_geo_admin.py
+++ nw_ow_locator/core/filters/map_geo_admin.py
@@ -15,8 +15,13 @@ def map_geo_admin_url(
         "returnGeometry": "true",
         "lang": lang,
         "sr": crs,
-        # "true" will sort features by distance to the bbox center, which leads to unexpected results.
-        "sortbbox": "false",
+        # "true" will sort features by distance to the bbox center, which can lead to
+        # unexpected address results: Addresses that are less similar to the search term
+        # will be returned first. However, this is still producing more and better
+        # results than setting it to "false". For example, it retrieves parcels for
+        # search term "123", with "true", it wouldn't.
+        # It's important to reorder results after receiving the response.
+        "sortbbox": "true",
     }
     if bbox:
         base_params["bbox"] = bbox
