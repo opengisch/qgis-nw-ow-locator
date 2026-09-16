99697b2eb8939a82bcbb3a4ca0c85c0b88ed7514
Pi Moll <patricia@opengis.ch>
2026-09-16 13:46:08 +0200

    Change URL sortbox parameter to "false" after GeoAdmin fix to receive results based on similarity to search term

--- nw_ow_locator/core/filters/map_geo_admin.py
+++ nw_ow_locator/core/filters/map_geo_admin.py
@@ -15,13 +15,8 @@ def map_geo_admin_url(
         "returnGeometry": "true",
         "lang": lang,
         "sr": crs,
-        # "true" will sort features by distance to the bbox center, which can lead to
-        # unexpected address results: Addresses that are less similar to the search term
-        # will be returned first. However, this is still producing more and better
-        # results than setting it to "false". For example, it retrieves parcels for
-        # search term "123", with "true", it wouldn't.
-        # It's important to reorder results after receiving the response.
-        "sortbbox": "true",
+        # We don't want to sort results by distance from the bbox center but similarity to the search term
+        "sortbbox": "false",
     }
     if bbox:
         base_params["bbox"] = bbox
